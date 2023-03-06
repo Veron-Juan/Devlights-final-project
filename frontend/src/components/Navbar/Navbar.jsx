aec01cb96bccbbea4ddc8a43b7704c4457cbcbe2
Matias Benites <matiasbenites665@gmail.com>
2023-03-05 21:13:50 -0300

    feat:Added commits;2nd preview launched;not finished

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,15 +3,18 @@ import Logo from "../../assets/logo.svg";
 // import { Link } from "react-router-dom";
 import Dropdown from "./Dropdown";
 
+// Component Navbar momentáneamente estático, con propiedad blur-effect
+
 const Navbar = () => {
   return (
-    <nav className="bg-gradient-to-l from-[#DEFFF8] w-full absolute z-10 top-0 backdrop-filter backdrop-blur-lg bg-opacity-30">
+    <nav className="w-screen sticky top-0 z-10 bg-white bg-cover backdrop-filter transparent backdrop-blur-sm bg-opacity-50">
       <div className="flex items-center text-black justify-around font-normal ">
         <div className="md:cursor-pointer -ml-10">
           <img src={Logo} alt="navbar-logo" className="md:cursor-pointer" />
+          {/* Falta añadir href al nametag del LOGO para regresar a home */}
         </div>
 
-        <ul className="md:flex hidden uppercase items-center font-[Poppins] text-xs gap-8 ">
+        <ul className="md:flex hidden uppercase items-center font-[Monserrat] text-xs gap-8 ">
           <li>
             {/* <Link to="" className=""> */}
             Mascotas cercanas
@@ -34,7 +37,7 @@ const Navbar = () => {
           </li>
         </ul>
 
-        {/* aaaaaa */}
+        {/* Falta incorporar navegación entre páginas una vez estén creadas */}
 
         <Dropdown />
       </div>
