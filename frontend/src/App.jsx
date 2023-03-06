34f2d3dd59be37ff0b3e07886c6d6ad3a6274a34
Matias Benites <matiasbenites665@gmail.com>
2023-03-05 21:11:18 -0300

    feat:Removed home's comp;Added just for testing a Register's comp to render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "../src/components/Navbar/Navbar";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-gradient-to-r from-yellow-100 via-white to-teal-200">
       <Navbar />
+      <Register />
     </div>
   );
 }
