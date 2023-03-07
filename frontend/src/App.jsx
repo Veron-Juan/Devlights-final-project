672969bb8fc7c47946041e29cea29d077bef49e5
Lucas Gauna <lks.gauna@gmail.com>
2023-03-07 17:26:05 -0300

    Merge con develop, carta "responsive" con el modal y mapa conectado a la api de google maps

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,51 @@
 import "./App.css";
 import Navbar from "../src/components/Navbar/Navbar";
 import Register from "./pages/register/Register";
+import Card from './components/Card'
+import Map from './components/MapComponent'
+
 
 function App() {
+  const lista = [
+    {
+    nombreUser: 'Juan',
+    tituloPost: 'Perro perdido',
+    ubicacionPost:  'Calle 123',
+    descripcionPost: 'Hola, mi perro se perdió ayer en la calle 123, si lo ven por favor contactarme al 123456789',
+    latitudPost: -27.465038847067884, 
+    longitudPost: -58.84456631035704},
+    {
+    nombreUser: 'Pedro',
+    tituloPost: 'Gato perdido',
+    ubicacionPost:  'Calle 123',
+    descripcionPost: 'Hola, mi gato se perdió ayer en la calle 123, si lo ven por favor contactarme al 123456789',
+    latitudPost: -27.460374137170163, 
+    longitudPost: -58.828945125378276},
+  ]
+
+  const location = {
+    address: '1600 Amphitheatre Parkway, Mountain View, california.',
+    lat: 37.42216,
+    lng: -122.08427,
+  }  
+  
+        
+
   return (
-    <div className="App bg-gradient-to-r from-yellow-100 via-white to-teal-200">
-      <Navbar />
-      <Register />
+    <>
+    <div className="App flex flex-row">
+
+    {lista.map((item) => (
+      <Card nombreUser={item.nombreUser} 
+      tituloPost={item.tituloPost} 
+      ubicacionPost={item.ubicacionPost} 
+      descripcionPost={item.descripcionPost}
+      latitudPost={item.latitudPost}
+      longitudPost={item.longitudPost} />
+    ))}
+      
     </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,67 @@
+import React,{useState} from "react";
+import Map from "./MapComponent";
+const urlImage = "https://img.freepik.com/vector-premium/perfil-hombre-dibujos-animados_18591-58482.jpg?w=2000"
+const urlPost = "https://estaticos-cdn.prensaiberica.es/clip/823f515c-8143-4044-8f13-85ea1ef58f3a_16-9-discover-aspect-ratio_default_0.jpg"
+//using tailwind
+// const Card =(props) => {
+//     return (
+//         <div className="bg-[#ecfeff] h-fit w-72 flex flex-col ">
+//             <div className= "h-fit w-full flex flex row">
+//                 <image src={props.imgUser} alt="imagen de perfil" className="h-20 w-20 rounded-full" /> 
+//                 <p className="text-2xl text-black font-bold">{props.nombreUser}</p>
+//             </div>
+//             <image src={props.imgPost} alt="imagen del post" className="h-20 w-20 " />
+//             <div className="h-fit w-full bg-blue flex flex-col justify-center">
+//                 <p className="text-2xl text-black font-bold">{props.tituloPost}</p>
+//                 <p className="text-xl text-black font-bold">{props.ubicacion}</p>
+//             </div>
+//         </div>
+//     );
+// }
+
+const Card =(props) => {
+    const [showModal, setShowModal] = useState(false);
+    const location = {
+      address: '1600 Amphitheatre Parkway, Mountain View, california.',
+      lat: 37.42216,
+      lng: -122.08427,
+    };  
+    return(
+    <>
+        <div className="container flex flex-col bg-white rounded-md max-w-sm h-auto w-72 mx-2">
+            <div className="flex flex-row h-auto">
+                <img src={urlImage} className="h-5 w-5 rounded-full my-2 mx-1 "/>
+                <p className="font-semibold text-black my-1.5">{props.nombreUser}</p>
+            </div>
+            <img src={urlPost} className=""/>
+            <div>
+                <p className="font-bold text-black my-2 mx-1">{props.tituloPost}</p>
+                <p className="text-black my-2 mx-1 text-sm">{props.ubicacionPost}</p>
+                <button className="text-black bg-orange rounded-md p-1" onClick={() => setShowModal(true)}>Ver mas</button>
+            </div>
+        </div>
+        {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-row w-full  outline-none focus:outline-none">
+                {/* Parte izquierda */}
+                <div className="flex flex-col">
+                  asdaaaaaaaaaaaaaaaaaa
+                    <Map location={location} zoomLevel={17}/>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+    );
+}
+
+
+export default Card;
