231ca7d811fdf8baee0b0c05d76550ea14f6e2e8
Gastón <gastonj0082@gmail.com>
2023-03-09 12:29:03 -0300

    modifique el color de texto

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ import { HomeHero } from '../../components/Home/HomeHero'
 export default function Home() {
   return (
     <div>
-      <div className='min-h-full max-w-full lg:px-14 mx-auto flex '>
+      <div className='min-h-full max-w-full lg:px-14 mx-auto flex text-black '>
         <HomeBody />
         <HomeHero/>
       </div>
