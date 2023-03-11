b40c6bd488b9f160bbb7b8b2a5580f8cd8afbea1
Juan Verón <juan_juve97@hotmail.com>
2023-03-11 17:03:51 -0300

    modificado algo de front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,28 @@ import "./App.css";
 import Navbar from "../src/components/Navbar/Navbar";
 import Register from "./pages/register/Register";
 import Posts from "./pages/posts/Posts";
+import Home from "./pages/home/Home";
+import { Route, Routes } from "react-router-dom";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 
 
 function App() {
   return (
     <div className="App bg-gradient-to-r from-yellow-100 via-white to-teal-200">
+      <Provider store={store} >
       <Navbar />
-      <Register />
-      <Posts/>
+      {/* <Register />
+      <Posts/> */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/posts" element={<Posts/>}/>
+        <Route path="/*" element={<h2>NOT FOUND</h2>}/>
+      </Routes>
 
-      
+      </Provider>
     </div>
   );
 }
--- frontend/src/components/Home/HomeButton.jsx
+++ frontend/src/components/Home/HomeButton.jsx
@@ -1,4 +1,4 @@
-import "./HomeButton.css"
+
 export function HomeButton (){
     return <button className="rounded-md justify-between bg-yellow-HomeButtton HomeButton 
     w-48 h-16 left-495 top-687 font-[Roboto] not-italic text-black font-extrabold text-base " type="button" >
