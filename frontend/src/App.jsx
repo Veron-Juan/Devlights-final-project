1a534df7fd0a818014d665eb9954935794290f05
Gastón <gastonj0082@gmail.com>
2023-03-14 12:07:30 -0300

    agregue el component de registro del post

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Navbar from "../src/components/Navbar/Navbar";
 import Register from "./pages/register/Register";
+import PostPage from "./pages/post/PostPage"
 
 function App() {
   return (
     <div className="App bg-gradient-to-r from-yellow-100 via-white to-teal-200">
       <Navbar />
-      <Register />
+      <PostPage />
     </div>
   );
 }
