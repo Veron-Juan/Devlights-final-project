741d4831b44d26d5517b2b38e3e3e5818dba96cc
Lucas Gauna <lks.gauna@gmail.com>
2023-03-25 15:00:43 -0300

    se arreglo un closing tag

--- frontend/src/components/Post/PostForm.jsx
+++ frontend/src/components/Post/PostForm.jsx
@@ -131,12 +131,68 @@ export function PostForm() {
       className="mt-[85px] flex flex-row font-['Montserrat'] not-italic "
       onSubmit={handleSubmit}
     >
-    <div className="flex flex-col">
-        <h1 className="mb-[10px] font-extrabold text-4xl min-w-full">
-          Publicar un aviso
-        </h1>
-        <hr className=" mb-[50px] bg"></hr>
-        <label className="font-extrabold  text-4xl">Nombre de la mascota</label>
+      <div className="flex flex-col">
+          <h1 className="mb-[10px] font-extrabold text-4xl min-w-full">
+            Publicar un aviso
+          </h1>
+          <hr className=" mb-[50px] bg"></hr>
+          <label className="font-extrabold  text-4xl">Nombre de la mascota</label>
+          <br />
+          <div
+            className="mb-[10px] border-b-4 pb-[7px] border-b-yellow-200 focus-within:border-yellow-400 
+                py-2  w-[49%] "
+          >
+            <input
+              className="mt-[10px] bg-white rounded-md"
+              type="text"
+              id="name"
+              name="name"
+              value={inputs.name}
+              onChange={handleInputChange}
+            ></input>
+            <br />
+          </div>
+
+        <label className="font-medium text-xl items-end">
+          Numero de contacto:
+        </label>
+        <br />
+        <div
+          className="mb-[10px] border-b-4 pb-[7px] border-b-yellow-200 focus-within:border-yellow-400
+              py-2  w-[49%] "
+        >
+          <input
+            className="mt-[10px] bg-white rounded-md"
+            type="text"
+            id="contact"
+            name="contact"
+            value={inputs.contact}
+            onChange={handleInputChange}
+          ></input>
+          <br />
+        </div>
+        <label className=" font-medium text-xl items-end">
+          Descripcion detallada:
+        </label>
+        <br />
+        <div
+          className=" mb-[30px] border-b-4 pb-[7px] border-b-yellow-200 focus-within:border-yellow-400 
+          py-2 lg:w-[89%] md:w-[89%] sm:w-[72%] w-[75%]"
+        >
+          <textarea
+            className=" mt-[10px] min-h-[100px] w-[310px] lg:min-h-[200px] md:min-w-[385px] 
+            lg:min-w-[400px] bg-white rounded-md"
+            type="text"
+            id="description"
+            name="description"
+            value={inputs.description}
+            onChange={handleInputChange}
+          ></textarea>
+          <br />
+        </div>
+        <label className="font-medium text-xl items-end ">
+          Zona donde fue visto por ultima vez:
+        </label>
         <br />
         <div
           className="mb-[10px] border-b-4 pb-[7px] border-b-yellow-200 focus-within:border-yellow-400 
@@ -145,103 +201,48 @@ export function PostForm() {
           <input
             className="mt-[10px] bg-white rounded-md"
             type="text"
-            id="name"
-            name="name"
-            value={inputs.name}
+            id="location"
+            name="location"
+            value={inputs.location}
             onChange={handleInputChange}
           ></input>
           <br />
         </div>
-
-      <label className="font-medium text-xl items-end">
-        Numero de contacto:
-      </label>
-      <br />
-      <div
-        className="mb-[10px] border-b-4 pb-[7px] border-b-yellow-200 focus-within:border-yellow-400
-            py-2  w-[49%] "
-      >
-        <input
-          className="mt-[10px] bg-white rounded-md"
-          type="text"
-          id="contact"
-          name="contact"
-          value={inputs.contact}
-          onChange={handleInputChange}
-        ></input>
-        <br />
-      </div>
-      <label className=" font-medium text-xl items-end">
-        Descripcion detallada:
-      </label>
-      <br />
-      <div
-        className=" mb-[30px] border-b-4 pb-[7px] border-b-yellow-200 focus-within:border-yellow-400 
-        py-2 lg:w-[89%] md:w-[89%] sm:w-[72%] w-[75%]"
-      >
-        <textarea
-          className=" mt-[10px] min-h-[100px] w-[310px] lg:min-h-[200px] md:min-w-[385px] 
-          lg:min-w-[400px] bg-white rounded-md"
-          type="text"
-          id="description"
-          name="description"
-          value={inputs.description}
-          onChange={handleInputChange}
-        ></textarea>
-        <br />
-      </div>
-      <label className="font-medium text-xl items-end ">
-        Zona donde fue visto por ultima vez:
-      </label>
-      <br />
-      <div
-        className="mb-[10px] border-b-4 pb-[7px] border-b-yellow-200 focus-within:border-yellow-400 
-            py-2  w-[49%] "
-      >
-        <input
-          className="mt-[10px] bg-white rounded-md"
-          type="text"
-          id="location"
-          name="location"
-          value={inputs.location}
-          onChange={handleInputChange}
-        ></input>
-        <br />
-      </div>
-      <div className=" mt-[30px] flex">
-        <div className="p-4 max-w-[100px] max-h-[100px ] flex items-center flex-col">
-          <div className="relative mb-6">
-            <input
-              className="relative z-[2] max-w-[90px] h-[90px]  cursor-pointer border-none 
-                        opacity-0"
-              type="file"
-              id="img"
-              name="testImage"
-              onChange={(event) =>
-                setInputs({ ...inputs, testImage: event.target.files[0] })
-              }
-            ></input>
-            <button
-              className="absolute flex top-0 left-0 w-full h-full z-[1] justify-center 
-                        items-center cursor-pointer outline-none"
-            >
-              <i>
-                <img src={iconoFoto} alt="img-button" className="" />
-              </i>
-            </button>
+        <div className=" mt-[30px] flex">
+          <div className="p-4 max-w-[100px] max-h-[100px ] flex items-center flex-col">
+            <div className="relative mb-6">
+              <input
+                className="relative z-[2] max-w-[90px] h-[90px]  cursor-pointer border-none 
+                          opacity-0"
+                type="file"
+                id="img"
+                name="testImage"
+                onChange={(event) =>
+                  setInputs({ ...inputs, testImage: event.target.files[0] })
+                }
+              ></input>
+              <button
+                className="absolute flex top-0 left-0 w-full h-full z-[1] justify-center 
+                          items-center cursor-pointer outline-none"
+              >
+                <i>
+                  <img src={iconoFoto} alt="img-button" className="" />
+                </i>
+              </button>
+            </div>
           </div>
+          <img
+            src={imgPreview}
+            alt="imagen"
+            className=" ml-12 rounded-md border-4 border-yellow-200
+            w-[120px] h-[120px] "
+          ></img>
         </div>
-        <img
-          src={imgPreview}
-          alt="imagen"
-          className=" ml-12 rounded-md border-4 border-yellow-200
-          w-[120px] h-[120px] "
-        ></img>
       </div>
-      <PostButton />
-      <div className="w-[30vw] h-[50vh]">
-        <MapComponent Center={Center} Marcadores={Marcadores} selecionMarcador={true} setInputs={setInputs}/>
-      </div>
+        <PostButton />
+        <div className="w-[30vw] h-[50vh]">
+          <MapComponent Center={Center} Marcadores={Marcadores} selecionMarcador={true} setInputs={setInputs}/>
+        </div>
     </form>
   );
 }
