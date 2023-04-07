f52d891ce4e616ce457dbcf5ea10af659e6e3fba
Matias Benites <matiasbenites665@gmail.com>
2023-04-07 13:46:28 -0300

    feat:Se añadió un min-width y se aplicó formato al código

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,30 +16,25 @@ import { AuthGuard } from "./guards/auth.guards";
 
 function App() {
   return (
-    <div className="App bg-gradient-to-r from-yellow-100 via-white to-teal-200">
-  
+    <div className="App min-h-[100vh] bg-gradient-to-r from-yellow-100 via-white to-teal-200">
       <Provider store={store}>
-     
         <Navbar />
-      
+
         {/* <Register />
       <Posts/> */}
         <Routes>
-        <Route path="/login" element={<Login />} />
-           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/posts" element={<Posts />} />
           <Route path="/*" element={<h2>NOT FOUND</h2>} />
           <Route path="/Test" element={<Test />} />
-          <Route element={<AuthGuard/>} >
-              <Route path="upload" element={<PostForm/> } />
+          <Route element={<AuthGuard />}>
+            <Route path="upload" element={<PostForm />} />
           </Route>
-
         </Routes>
-        
-      <Footer />
-      
-     
+
+        <Footer />
       </Provider>
     </div>
   );
